467b667e6a18a29633aded9a872c34ce5c39e15b
Retrojb <baltes.26@osu.edu>
2018-09-06 14:01:19 -0400

    inital build

---
+++ src/main/java/io/retrojb/github/Time2Dine/Time2DineApplication.java
@@ -0,0 +1,12 @@
+package io.retrojb.github.Time2Dine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Time2DineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Time2DineApplication.class, args);
+	}
+}
---
+++ src/test/java/io/retrojb/github/Time2Dine/Time2DineApplicationTests.java
@@ -0,0 +1,16 @@
+package io.retrojb.github.Time2Dine;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Time2DineApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
